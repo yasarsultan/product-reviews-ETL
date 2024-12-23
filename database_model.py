6e61d11bd21a22be3039af85dcffd8c3488eeb6b
yasarsultan <yasersultan03@gmail.com>
2024-12-23 13:22:45 +0530

    Update load function changes

---
+++ database_model.py
@@ -0,0 +1,64 @@
+import os
+from psycopg2 import connect, sql
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def getConnection():
+    connection = connect(
+        dbname = os.getenv('DB_NAME'),
+        user = os.getenv('DB_USER'),
+        password = os.getenv('DB_PASS'),
+        host = "localhost",
+        port = "5432"
+    )
+    connection.autocommit = True
+
+    return connection
+
+
+def createTable():
+    connection = getConnection()
+    cursor = connection.cursor()
+    print("Creating table...")
+    
+    cursor.execute(
+        sql.SQL(
+            """
+            CREATE TABLE IF NOT EXISTS reviews(
+                review_id NUMERIC PRIMARY KEY,
+                product_id NUMERIC NOT NULL,
+                customer_id NUMERIC NOT NULL,
+                rating DECIMAL NOT NULL,
+                review_date DATE NOT NULL,
+                review_text TEXT,
+                sentiment_score DECIMAL)
+            """
+        )
+    )
+    cursor.close()
+    connection.close()
+
+    print("Table create successfully.")
+
+
+def insertData(df):
+    connection = getConnection()
+    cursor = connection.cursor()
+    print("Inserting data into table...")
+
+    for index, row in df.iterrows():
+        cursor.execute(
+            sql.SQL(
+                """
+                INSERT INTO reviews(review_id, product_id, customer_id, rating, review_date, review_text, sentiment_score)
+                VALUES(%s, %s, %s, %s, %s, %s, %s)
+                """
+            ),
+            (row['review_id'], row['product_id'], row['customer_id'], row['rating'], row['review_date'], row['review_text'], row['sentiment_score'])
+        )
+    cursor.close()
+    connection.close()
+
+    print("Inserted data successfully.")
+
--- etl.py
+++ etl.py
@@ -1,4 +1,10 @@
 import pandas as pd
+from textblob import TextBlob
+from database_model import createTable, insertData
+
+
+def getSentimentScore(comment):
+    return TextBlob(str(comment)).sentiment.polarity
 
 
 def extract(source):
@@ -11,37 +17,47 @@ def extract(source):
         return df
 
     elif source == 'sourceC':
-        df = pd.DataFrame(columns=["review_id","product_id","customer_id","rating","review_date"])
+        df = pd.DataFrame(columns=["review_id","product_id","customer_id","rating","review_date", "review_text"])
 
         with open("sourceC/reviews.txt", "r") as file:
             content = file.readlines()
         
         for review in content:
             row = review.split("|")
-            row[-1] = str(row[-1][1:-2])
+            row[5] = str(row[5][:-1])
             df = df._append(pd.Series(row, index=df.columns), ignore_index=True)
 
         return df
 
-def transform():
-    pass
 
-def load():
-    pass
+def transform(df1, df2, df3):
+    df = pd.concat([df1, df2, df3], ignore_index=True)
+    df = df[["review_id", "product_id", "customer_id", "rating", "review_date", "review_text"]]
+
+    df = df.drop_duplicates(subset=['review_id'])
+    df = df.dropna(subset=['rating'])
+
+    df['review_date'] = pd.to_datetime(df['review_date']).dt.strftime('%Y-%m-%d')
+
+    df['sentiment_score'] = df['review_text'].apply(getSentimentScore)
+
+    return df
+
+
+def load(df):
+    createTable()
+    insertData(df)
+
 
 
 def main():
     df1 = extract('sourceA')
-    # print(df1.head())
-
     df2 = extract('sourceB')
-    # print(df2.head())
-
     df3 = extract('sourceC')
-    # print(df2.head())
 
-    df = pd.concat([df1, df2, df3], ignore_index=True)
+    df = transform(df1, df2, df3)
 
-    print(df.head(10))
+    load(df)
+
 
 main()
